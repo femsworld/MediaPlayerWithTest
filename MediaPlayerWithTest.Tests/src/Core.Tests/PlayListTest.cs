baa3ae6c2522d2fc5507d5fa4d65ac9a6c014db3
femsworld <femsworld@gmail.com>
2023-07-01 18:45:47 +0300

    Added more test and MyMediaFile

--- MediaPlayerWithTest.Tests/src/Core.Tests/PlayListTest.cs
+++ MediaPlayerWithTest.Tests/src/Core.Tests/PlayListTest.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -20,8 +21,44 @@ namespace MediaPlayerWithTest.Tests.src.Core.Tests
         {
             var playList = new PlayList("Fela Kuti", 1);
             Assert.Equal("Fela Kuti", playList.ListName);
+
+        }
+
+        [Theory]
+        [MemberData(nameof(TestUpdatePlayListData))]
+        // public void AddNewFile_ValidData_NewFileAdded(string name, int userId, string? updatedName, int? updatedUserId, string newName, int newUserId)
+        public void AddNewFile_ValidData_NewFileAdded(string name, int userId, string? updatedName, int? updatedUserId, string newName, int newUserId)
+        {
+            var playList = new PlayList(name, userId);
+            var updatedFile = new MyMediaFile(updatedName, "FilePath", TimeSpan.Zero);
+
+            // playList.AddNewFile(updatedName, updatedUserId);
+            playList.AddNewFile(updatedFile, (int)updatedUserId);
+
+            Assert.Equal(playList.ListName, newName);
+        }
+
+        public static IEnumerable<object[]> TestUpdatePlayListData
+        {
+            get
+            {
+                yield return new object[] { "Fela Kuti", 1, "Fela Kuti", 2, "Fela Kuti", 3 };
+                yield return new object[] { "Femi Kuti", 1, "Femi Kuti", 2, "Femi Kuti", 3 };
+            }
         }
     }
 
+    public class TestUpdatePlayListData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { "Fela Kuti", 1, "Fela Kuti", 2, "Fela Kuti", 3 };
+            yield return new object[] { "Femi Kuti", 1, "Femi Kuti", 2, "Femi Kuti", 3 };
+        }
 
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
 }
