c4bfba89ec1f333e89a1b693f0e0b5674a1e1f54
femsworld <femsworld@gmail.com>
2023-07-01 19:54:06 +0300

    Updated UserTest

--- MediaPlayerWithTest.Tests/src/Core.Tests/UserTest.cs
+++ MediaPlayerWithTest.Tests/src/Core.Tests/UserTest.cs
@@ -19,55 +19,44 @@ namespace MediaPlayerWithTest.Tests.src.Core.Tests
         [Fact]
         public void AddNewList_ValidList_ListAddedToLists()
         {
-            // Arrange
             var user = User.Instance;
             var list = new PlayList("My List", 1);
 
-            // Act
             user.AddNewList(list);
 
-            // Assert
             Assert.Contains(list, user.Lists);
         }
 
         [Fact]
         public void RemoveOneList_ExistingList_ListRemovedFromLists()
         {
-            // Arrange
             var user = User.Instance;
             var list = new PlayList("My List", 1);
             user.AddNewList(list);
 
-            // Act
             user.RemoveOneList(list);
 
-            // Assert
             Assert.DoesNotContain(list, user.Lists);
         }
 
-        // [Fact]
-        // public void EmptyOneList_ExistingList_ListEmptied()
-        // {
-        //     // Arrange
-        //     var user = User.Instance;
-        //     var list = new PlayList("My List", 1);
-        //     user.AddNewList(list);
+        [Fact]
+        public void EmptyOneList_ExistingList_ListEmptied()
+        {
+            var user = User.Instance;
+            var list = new PlayList("My List", 1);
+            user.AddNewList(list);
 
-        //     // Act
-        //     user.EmptyOneList(list);
+            user.EmptyOneList(list);
 
-        //     // Assert
-        //     Assert.Empty(list.Files);
-        // }
+            Assert.Empty(list.Files);
+        }
 
         [Fact]
         public void EmptyOneList_NonExistingList_ArgumentNullExceptionThrown()
         {
-            // Arrange
             var user = User.Instance;
             var nonExistingList = new PlayList("Non-existing List", 2);
-
-            // Act and Assert
+            
             Assert.Throws<ArgumentNullException>(() => user.EmptyOneList(nonExistingList));
         }
     }
