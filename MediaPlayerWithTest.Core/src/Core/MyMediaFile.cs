30c9ea5ad842ff5ea2bb2b6a3db62ef598540fa3
femsworld <femsworld@gmail.com>
2023-07-02 19:40:51 +0300

    Updated the MediaFileTest and MyMediaFiles

--- MediaPlayerWithTest.Core/src/Core/MyMediaFile.cs
+++ MediaPlayerWithTest.Core/src/Core/MyMediaFile.cs
@@ -7,8 +7,17 @@ namespace MediaPlayerWithTest.Core.src.Core
 {
     public class MyMediaFile : MediaFile
     {
+        public bool IsPlaying { get; private set; }
+        public TimeSpan CurrentPosition { get; private set; }
         public MyMediaFile(string fileName, string filePath, TimeSpan duration) : base(fileName, filePath, duration)
         {
+            IsPlaying = false;
+            CurrentPosition = TimeSpan.Zero; 
+        }
+        public static bool IsValidPlaybackSpeed(double speed)
+        {
+            double[] validSpeeds = { 0.25, 0.5, 1, 1.25, 1.5, 1.75, 2 };
+            return validSpeeds.Contains(speed);
         }
     }
 }
