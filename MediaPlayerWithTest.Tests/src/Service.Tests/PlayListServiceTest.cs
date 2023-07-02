fb80d27759c9d9dfb95022fc544b475a51b4055e
femsworld <femsworld@gmail.com>
2023-07-02 17:10:00 +0300

    Added UserServiceTest and updated PlayListSeriveTest

--- MediaPlayerWithTest.Tests/src/Service.Tests/PlayListServiceTest.cs
+++ MediaPlayerWithTest.Tests/src/Service.Tests/PlayListServiceTest.cs
@@ -1,8 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Xunit;
+using Moq;
+using MediaPlayerWithTest.Core.src.RepositoryInterface;
+using MediaPlayerWithTest.Service.src.Service;
 
 namespace MediaPlayerWithTest.Tests.src.Service.Tests
 {
@@ -12,6 +10,56 @@ namespace MediaPlayerWithTest.Tests.src.Service.Tests
         public void Test1()
         {
             Assert.True(true);
+        }
+        [Fact]
+        public void AddNewFile_ValidInput_CallsAddNewFileMethodOnRepository()
+        {
+            var mockPlayListRepository = new Mock<IPlayListRepository>();
+            var playListService = new PlayListService(mockPlayListRepository.Object);
+            int playListId = 1;
+            int fileId = 2;
+            int userId = 3;
+
+            playListService.AddNewFile(playListId, fileId, userId);
+
+            mockPlayListRepository.Verify(
+                repo => repo.AddNewFile(playListId, fileId, userId),
+                Times.Once
+            );
+        }
+
+        [Fact]
+        public void EmptyList_ValidInput_CallsEmptyListMethodOnRepository()
+        {
+            var mockPlayListRepository = new Mock<IPlayListRepository>();
+            var playListService = new PlayListService(mockPlayListRepository.Object);
+            int playListId = 1;
+            int userId = 3;
+
+            playListService.EmptyList(playListId, userId);
+
+            mockPlayListRepository.Verify(
+                repo => repo.EmptyList(playListId, userId),
+                Times.Once
+            );
+        }
+
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(4, 5, 6)]
+        [InlineData(7, 8, 9)]
+        public void RemoveFile_ValidInput_CallsRemoveFileMethodOnRepository(int playListId, int fileId, int userId)
+        {
+            var mockPlayListRepository = new Mock<IPlayListRepository>();
+            var playListService = new PlayListService(mockPlayListRepository.Object);
+
+            playListService.RemoveFile(playListId, fileId, userId);
+
+            mockPlayListRepository.Verify(
+                repo => repo.RemoveFile(playListId, fileId, userId),
+                Times.Once
+            );
+
         }
     }
 }
---
+++ MediaPlayerWithTest.Tests/src/Service.Tests/UserServiceTest.cs
@@ -0,0 +1,97 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediaPlayerWithTest.Core.src.RepositoryInterface;
+using MediaPlayerWithTest.Service.src.Service;
+using Moq;
+using Xunit;
+
+namespace MediaPlayerWithTest.Tests.src.Service.Tests
+{
+    public class UserServiceTest
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+
+    [Fact]
+        public void AddNewList_ValidData_CallsRepositoryAddNewList()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var userService = new UserService(userRepositoryMock.Object);
+            var name = "My List";
+            var userId = 1;
+
+            userService.AddNewList(name, userId);
+
+            userRepositoryMock.Verify(x => x.AddNewList(name, userId), Times.Once);
+        }
+
+        [Fact]
+        public void EmptyOneList_ValidData_CallsRepositoryEmptyOneList()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var userService = new UserService(userRepositoryMock.Object);
+            var listId = 1;
+            var userId = 1;
+
+            userService.EmptyOneList(listId, userId);
+
+            userRepositoryMock.Verify(x => x.EmptyOneList(listId, userId), Times.Once);
+        }
+
+        [Fact]
+        public void GetAllList_ValidData_CallsRepositoryGetAllList()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var userService = new UserService(userRepositoryMock.Object);
+            var userId = 1;
+
+            userService.GetAllList(userId);
+
+            userRepositoryMock.Verify(x => x.GetAllList(userId), Times.Once);
+        }
+
+        [Fact]
+        public void GetListById_ValidData_CallsRepositoryGetListById()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var userService = new UserService(userRepositoryMock.Object);
+            var listId = 1;
+
+            userService.GetListById(listId);
+
+            userRepositoryMock.Verify(x => x.GetListById(listId), Times.Once);
+        }
+
+        [Fact]
+        public void RemoveAllLists_ValidData_CallsRepositoryRemoveAllLists()
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var userService = new UserService(userRepositoryMock.Object);
+            var userId = 1;
+
+            userService.RemoveAllLists(userId);
+
+            userRepositoryMock.Verify(x => x.RemoveAllLists(userId), Times.Once);
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 1)]
+        [InlineData(3, 2)]
+        public void RemoveOneList_ValidData_CallsRepositoryRemoveOneList(int listId, int userId)
+        {
+            var userRepositoryMock = new Mock<IUserRepository>();
+            var userService = new UserService(userRepositoryMock.Object);
+
+            userService.RemoveOneList(listId, userId);
+
+            userRepositoryMock.Verify(x => x.RemoveOneList(listId, userId), Times.Once);
+        }
+
+    }
+}
